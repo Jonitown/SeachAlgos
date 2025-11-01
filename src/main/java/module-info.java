1572c29915f79f109cf1e78940569a5e43a11d21
joniitown <jonathan.weber08@outlook.de>
2025-11-01 23:03:01 +0100

    h

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module de.jonitown.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens de.jonitown.demo to javafx.fxml;
+    exports de.jonitown.demo;
+}
